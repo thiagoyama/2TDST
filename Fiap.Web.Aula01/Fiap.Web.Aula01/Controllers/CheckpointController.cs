b3a5974bd7c9d03df48fa35199394aac8136e3d2
Thiago Yamamoto <thiagoyama@gmail.com>
2023-08-30 20:47:28 -0300

    ASP.NET

---
+++ Fiap.Web.Aula01/Fiap.Web.Aula01/Controllers/CheckpointController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula01.Controllers
+{
+    public class CheckpointController : Controller
+    {
+        public IActionResult Calcular()
+        {
+            return View();
+        }
+    }
+}
