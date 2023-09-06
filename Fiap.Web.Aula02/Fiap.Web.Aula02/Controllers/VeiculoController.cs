a4745052c42cbf3e189a972669f96b438d3f9339
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-09-06 20:51:03 -0300

    Views

---
+++ Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/VeiculoController.cs
@@ -0,0 +1,25 @@
+﻿using Fiap.Web.Aula02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        //Lista para gravar os veiculos
+        private static IList<Veiculo> _lista = new List<Veiculo>();
+
+        //TAREFA!
+        //Cadastrar o veiculo na lista
+        //Usar o index para exibir todos os veiculos em uma tabela HTML
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
