07396e5e6965e897aef9cbfbe93b535ff3541af3
Thiago Yamamoto <thiagoyama@gmail.com>
2023-09-28 22:45:40 -0300

    EF

---
+++ Fiap.Web.Aula03/Fiap.Web.Aula03/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using Fiap.Web.Aula03.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula03.Controllers
+{
+    public class FilmeController : Controller
+    {        
+        private StreamingContext _context;
+
+        //O contexto é injetado no construtor
+        public FilmeController(StreamingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
