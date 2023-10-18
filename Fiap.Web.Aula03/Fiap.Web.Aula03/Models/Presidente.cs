57bbf9dc3fa6536a29da1ed64c267972bea9df43
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-10-18 20:40:59 -0300

    Relacionamentos

---
+++ Fiap.Web.Aula03/Fiap.Web.Aula03/Models/Presidente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fiap.Web.Aula03.Models
+{
+    [Table("Tbl_Presidente")]
+    public class Presidente
+    {
+        [Column("Id")]
+        public int PresidenteId { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
