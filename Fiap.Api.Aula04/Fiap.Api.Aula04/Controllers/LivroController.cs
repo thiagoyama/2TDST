d39a942dd40757aaf0532952ef7ccdd03ee83b66
Thiago Yamamoto <thiagoyama@gmail.com>
2023-11-09 22:24:50 -0300

    API Rest

--- Fiap.Api.Aula04/Fiap.Api.Aula04/Controllers/LivroController.cs
+++ Fiap.Api.Aula04/Fiap.Api.Aula04/Controllers/LivroController.cs
@@ -2,6 +2,7 @@
 using Fiap.Api.Aula04.Persistencia;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fiap.Api.Aula04.Controllers
 {
@@ -14,6 +15,49 @@ namespace Fiap.Api.Aula04.Controllers
         public LivroController(LivrariaContext context)
         {
             _context = context;
+        }
+
+        //GET api/livro/1
+        [HttpGet("{id}")]
+        public ActionResult<Livro> Get(int id)
+        {
+            var livro = _context.Livros.Find(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return Ok(livro);
+        }
+
+        //PUT api/livro/1
+        [HttpPut("{id}")]
+        public ActionResult<Livro> Put(int id, Livro livro)
+        {
+            var busca = _context.Livros.Find(id);
+            if (busca == null)
+            {
+                return NotFound();
+            }
+            //Retira o objeto busca do gerenciamento do DbContext
+            _context.Entry<Livro>(busca).State = EntityState.Detached;
+            livro.Id = id;
+            _context.Livros.Update(livro);
+            _context.SaveChanges();
+            return Ok(livro);
+        }
+
+        //DELETE api/livro/1
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var livro = _context.Livros.Find(id);
+            if (livro  == null)
+            {
+                return NotFound();
+            }
+            _context.Livros.Remove(livro);
+            _context.SaveChanges();
+            return NoContent();
         }
 
         [HttpGet]
